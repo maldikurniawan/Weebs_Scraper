66c97e79b199c7829eed58c6da535a3d65ad7f23
maldikurniawan <akurniawan2102@gmail.com>
2025-08-19 15:46:11 +0700

    fix index

---
+++ vercel-index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+
+/// Use Komikcast route
+app.use("/api/komikcast/", require("../routes/manga-route.js"));
+/// Use Komiku route
+app.use("/api/komiku/", require("../routes/komiku-route.js"));
+/// Use Anoboy route
+app.use("/api/anoboy/", require("../routes/anoboy-route.js"));
+/// Use Proxy route
+app.use("/api/proxy/", require("../routes/proxy-route.js"));
+
+// ⬅️ ekspor handler untuk vercel
+module.exports = app;
