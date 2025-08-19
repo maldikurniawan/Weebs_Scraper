97957dc94ea479c758a8527b63e6a5cf3f952b1b
maldikurniawan <akurniawan2102@gmail.com>
2025-08-19 15:49:21 +0700

    fix index

--- weebs-api-index.js
+++ weebs-api-index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
-const cors = require("cors");
 
 dotenv.config();
 
@@ -11,8 +10,6 @@ const port = process.env.PORT || 3000;
 app.use(morgan("dev"));
 
 app.use(express.json());
-
-app.use(cors());
 
 /// Use Komikcast route
 app.use("/api/komikcast/", require("./routes/manga-route.js"));
